d79a0ce07fb2d3411f5d704a23d0d0921c590151
nesterone <igornester@gmail.com>
2024-07-13 10:20:30 +0200

    - new message object

--- src/main/java/org/yura/utils/Message.java
+++ src/main/java/org/yura/utils/Message.java
@@ -4,6 +4,47 @@ package org.yura.utils;
  * The {@code Messages} class provides utility methods for processing and validating chat messages.
  */
 public class Message {
+
+    private String text;
+    private String to;
+    private String from;
+
+
+    public Message(String text, String to, String from) {
+        this.text = text;
+        this.to = to;
+        this.from = from;
+    }
+
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+
+
+
     public static String reverseMsg(String msg){
         String from = msg.split(":")[0];
         String to = msg.split(":")[1];
---
+++ src/test/java/org/yura/utils/MessageTest.java
@@ -0,0 +1,22 @@
+package org.yura.utils;
+
+import org.yura.model.MessageTransfer;
+import org.yura.model.Player;
+import org.yura.single.process.chat.BlockingQueueTransfer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MessageTest {
+
+    @org.junit.jupiter.api.Test
+    void createMessage() {
+
+        String text = "foo";
+        String to = "ivan";
+        String from = "fedor";
+
+        Message message = new Message(text, to, from);
+
+        assertEquals(message.getFrom(), from);
+    }
+}
